884ec8398c1c5dde9d85557448a1fb5d064d4a90
ibayeye <169161293+ibayeye@users.noreply.github.com>
2025-02-02 13:19:58 +0700

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,19 +8,24 @@ const Footer = () => {
       <footer className="footer bg-[#2D2D39] font-title p-10 ">
         <nav>
           <h6 className="footer-title">Services</h6>
-          <a className="link link-hover">Web Development</a>
-          <a className="link link-hover">Mobile App Development</a>
-          <a className="link link-hover">Design</a>
+          <a href="#services" className="link link-hover">
+            Web Development
+          </a>
+          <a href="#services" className="link link-hover">Mobile App Development</a>
+          <a href="#services" className="link link-hover">Design</a>
         </nav>
         <nav>
           <h6 className="footer-title">About</h6>
-          <a className="link link-hover">My History</a>
-          <a className="link link-hover">Contact</a>
+          <a href="#home" className="link link-hover">My History</a>
+          <a href="#contact" className="link link-hover">Contact</a>
         </nav>
         <nav>
           <h6 className="footer-title">Social</h6>
           <div className="grid grid-flow-col gap-4">
-            <a href="https://www.linkedin.com/in/iqbal-ramadan-1aa02834a/" target="_blank">
+            <a
+              href="https://www.linkedin.com/in/iqbal-ramadan-1aa02834a/"
+              target="_blank"
+            >
               <FaLinkedin className="h-6 w-6 cursor-pointer" />
             </a>
             <a href="https://www.instagram.com/iqbalramadann_/" target="_blank">
